0d8bdcffcd41919ac0c606e8d824c3141a8505ba
suyashbuilds <srivastavsuyash903@gmail.com>
2026-09-11 22:47:51 +0530

    Time: 0 ms (100%), Space: 12.4 MB (95.32%) - LeetHub

---
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        
+        vector<int> mp(10, 0);
+        vector<int> ans;
+
+        for(int &x : digits){
+            mp[x]++;
+        }
+
+        for(int i=1; i<=9; i++){
+            if(mp[i] == 0){
+                continue;
+            }
+            mp[i]--;
+            for(int j =0; j<=9; j++){
+                if(mp[j] == 0){
+                    continue;
+                }
+                mp[j]--;
+                for(int k=0; k<=8; k+=2){
+                    if(mp[k] == 0){
+                        continue;
+                    }
+                    mp[k]--;
+
+                    int num = i*100 + j*10 + k;
+                    ans.push_back(num);
+                    mp[k]++;
+                }
+                mp[j]++;
+            }
+            mp[i]++;
+        }
+        return ans;
+    }
+};
